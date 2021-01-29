88a12f413b697bad8cb208870e945845b776d269
Herson Geovanni Lopez Campos <usis031120@ugb.edu.sv>
2021-01-29 10:36:06 -0600

    Desarrollo de la calculadora basica con Radio Group y Spinner

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -1,6 +1,12 @@
 package com.ugb.miprimerproyecto;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcular(View view) {
+        TextView tempVal = (TextView)findViewById(R.id.txtnum1);
+        double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = (TextView)findViewById(R.id.txtnum2);
+        double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = 0;
+        RadioButton optOperacionesBasicas = findViewById(R.id.optSuma);
+        if ( optOperacionesBasicas.isChecked() ) {
+           respuesta = num1 + num2;
+       }
+        optOperacionesBasicas = findViewById(R.id.optResta);
+        if ( optOperacionesBasicas.isChecked() ) {
+            respuesta = num1 - num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optMultiplicacion);
+        if ( optOperacionesBasicas.isChecked() ) {
+            respuesta = num1 * num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optDivision);
+        if ( optOperacionesBasicas.isChecked() ) {
+            respuesta = num1 / num2;
+        }
+        optOperacionesBasicas = findViewById(R.id.optFactorial);
+        if ( optOperacionesBasicas.isChecked() ) {
+            for (int i=2; i<=num1; i++){
+                respuesta *=i;
+            }
+        }
+
+        Spinner cboOperacionesBasicas = findViewById(R.id.cboOperacionesBasicas);
+        switch (cboOperacionesBasicas.getSelectedItemPosition()){
+            case 0:
+                respuesta = num1+num2;
+                break;
+            case 1:
+                respuesta = num1-num2;
+                break;
+            case 2:
+                respuesta = num1*num2;
+                break;
+            case 3:
+                respuesta = num1/num2;
+                break;
+        }
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: " + respuesta);
     }
 }
